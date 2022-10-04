27f879814ef72d41ffe8df8e6b5ddc2ab9c8f8b2
Sergey <sergeyfedorenko.a89@gmail.com>
2022-10-04 15:25:54 +0300

    chast 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int enk = 4;
+string[] CreateArray()
+{
+    string[] enkArray = new string[size];
+    for(int i = 0; i < size; i = i + 1)
+    {
+        Console.Write($"Введите элемент {i + 1}: ");
+        enkArray[i] = Console.ReadLine();
+    }
+    return enkArray;
+}
